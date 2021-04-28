46844ce59b7df9f5fb8d16747fb9069fb509b2ad
Adam Trossing <Adam@trossing.se>
2021-04-28 09:00:23 +0200

    Fixade så att det ritades en grid

--- spelet.js
+++ spelet.js
@@ -1,18 +1,72 @@
-const container = document.getElementById("container");
 
-function makeRows(rows, cols) { //gör en grid 
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (r = 1; r <= (rows); r++) {
-      for(c=1;c <= cols; c++){
-    let cell = document.createElement("div");
-    cell.setAttribute("data-X", c )
-    cell.setAttribute("data-Y", r )
-    container.appendChild(cell).className = "grid-item";
+
+let keysPressed = {}
+
+document.addEventListener("keydown", (event) => {
+
+  keysPressed[event.key] = true;
+});
+document.addEventListener("keyup", (event) => {
+
+  keysPressed[event.key] = true;
+});
+
+let GameCanvas = document.getElementById("tutorial"); 
+let GameCanvasContext = GameCanvas.getContext('2d');
+
+GameCanvasContext.background = "#000000";
+
+class Rectangle {
+  constructor(x, y, height, width, color) {
+    this.x = x
+    this.y = y
+    this.height = height
+    this.width = width
+    this.color = color
+    this.xmom = 0
+    this.ymom = 0
   }
-  };
-};
-makeRows(10, 10); //hur stor griden är
+  draw() {
+    GameCanvasContext.lineWidth = 1
+    GameCanvasContext.fillStyle = this.color
+    GameCanvasContext.strokeStyle = "black"
+    GameCanvasContext.fillRect(this.x, this.y, this.width, this.height)
+    GameCanvasContext.strokeRect(this.x, this.y, this.width, this.height)
+  }
+  move() {
+    this.x += this.xmom
+    this.y += this.ymom
+  }
+}
+class Grid {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.x = {};
+    this.y = {};
+    this.blocks = [];
+    for(let l = 0; this.y <GameCanvas.height; l++){
+      for(let l = 0; this.x <GameCanvas.length; l++){
+        let block = new Rectangle(this.x, this.y, this.width, this.height, color)
+      
+      }
+      this.block.push(block);
+      this.x += this.width;
+    }
+this.y += this.height;
+this.x = {}
 
-let player = document.createElement("div");
+}
+
+draw() {
+  for (let b = 0; b < this.blocks.length; b++) {
+    this.blocks[b].draw()
+  }
+}
+}
+let board = new Grid(70,70,"green")
+
+window.setInterval(function(){
+board.draw();
+})
 
